03015fbff98614e554eefbbb2ea2bca552963d0d
randymcmillan <randy.lee.mcmillan@gmail.com>
2021-11-11 12:54:06 -0500

    getcoins.py: reporting and reachable

--- getcoins.py
+++ getcoins.py
@@ -9,6 +9,8 @@ import subprocess
 import requests
 import sys
 import json
+from pprint import pprint
+from glom import glom
 
 sys.path.append('.')
 sys.path.append("/usr/local/lib/python3.7/site-packages")
@@ -20,8 +22,26 @@ parser = argparse.ArgumentParser(description='Script to get coins from a faucet.
 parser.add_argument('-c', '--cmd', dest='cmd', default='docker', help='bitcoin-cli command to use')
 parser.add_argument('-f', '--faucet', dest='faucet', default='http://signet.xenon.fun:5000/faucet', help='URL of the faucet')
 parser.add_argument('-a', '--address', dest='address', default='', help='Bitcoin address to which the faucet should send')
+parser.add_argument('-r', '--report', dest='report', default='false', help='Return session data')
+parser.add_argument('-s', '--success', dest='success', default='false', help='return 0 if true')
+parser.add_argument('-res', '--response', dest='response', default='false', help='Return response from faucet')
 
 args = parser.parse_args()
+
+def print_report():
+    #print(data)
+    if args.report == 'true':
+        # NOTE: jq expected syntax
+        #  echo  {\"address\": \"tb1qhjd4lxv7vkqp7pen34t8zfxvvc8f8eqpppug26\"} | jq .
+        print(data) #return session data first!
+        if res == '<Response [200]>':
+            try:
+                print(data)
+            except:
+                print('Printing session data failed.')
+                exit()
+        if res.text != 'Success':
+            print(res)
 
 if args.address == '':
     # get address for receiving coins
@@ -30,7 +50,15 @@ if args.address == '':
     data = {'address': args.address}
 try:
     res = requests.get(args.faucet, params=data)
+    print_report()
+    if res.text != 'Success':
+        print(res)
 except:
     print('Unexpected error when contacting faucet:', sys.exc_info()[0])
     exit()
-print(res.text)
+
+if args.response == 'true':
+    print(res.text)
+    if res.text != 'Success':
+        print(res)
+
