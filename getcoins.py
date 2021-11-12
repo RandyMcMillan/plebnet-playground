a33bcab6be58444b0cb426c0a88a4f6a1c303f6d
randymcmillan <randy.lee.mcmillan@gmail.com>
2021-11-11 23:17:43 -0500

    getcoins.py: return Success ot report address sent to

--- getcoins.py
+++ getcoins.py
@@ -13,10 +13,8 @@ from pprint import pprint
 from glom import glom
 
 sys.path.append('.')
-sys.path.append("/usr/local/lib/python3.7/site-packages")
 sys.path.append("/usr/local/lib/python3.8/site-packages")
 sys.path.append("/usr/local/lib/python3.9/site-packages")
-#print(sys.path)
 
 parser = argparse.ArgumentParser(description='Script to get coins from a faucet.')
 parser.add_argument('-c', '--cmd', dest='cmd', default='docker', help='bitcoin-cli command to use')
@@ -24,41 +22,29 @@ parser.add_argument('-f', '--faucet', dest='faucet', default='http://signet.xeno
 parser.add_argument('-a', '--address', dest='address', default='', help='Bitcoin address to which the faucet should send')
 parser.add_argument('-r', '--report', dest='report', default='false', help='Return session data')
 parser.add_argument('-s', '--success', dest='success', default='false', help='return 0 if true')
-parser.add_argument('-res', '--response', dest='response', default='false', help='Return response from faucet')
 
 args = parser.parse_args()
 
 def print_report():
-    #print(data)
     if args.report == 'true':
+        print(args.address)
         # NOTE: jq expected syntax
-        #  echo  {\"address\": \"tb1qhjd4lxv7vkqp7pen34t8zfxvvc8f8eqpppug26\"} | jq .
-        print(data) #return session data first!
-        if res == '<Response [200]>':
-            try:
-                print(data)
-            except:
-                print('Printing session data failed.')
-                exit()
-        if res.text != 'Success':
-            print(res)
+        # echo  {\"address\": \"tb1qhjd4lxv7vkqp7pen34t8zfxvvc8f8eqpppug26\"} | jq .
+        #print(data) #return session data first!
+    else:
+        print(res.text)
 
 if args.address == '':
     # get address for receiving coins
     args.address = json.loads(subprocess.check_output([args.cmd] + ['exec','-it','playground-lnd', 'lncli', '--macaroonpath', '/root/.lnd/data/chain/bitcoin/signet/admin.macaroon', 'newaddress', 'p2wkh']))["address"]
-
     data = {'address': args.address}
+else:
+    data = {'address': args.address}
+
 try:
     res = requests.get(args.faucet, params=data)
     print_report()
-    if res.text != 'Success':
-        print(res)
 except:
     print('Unexpected error when contacting faucet:', sys.exc_info()[0])
     exit()
-
-if args.response == 'true':
-    print(res.text)
-    if res.text != 'Success':
-        print(res)
 
