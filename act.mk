a923f225a04cad6f57ca33556e3aedfd7c872df5
randymcmillan <randy.lee.mcmillan@gmail.com>
2023-10-13 08:51:39 -0400

    act.mk:docker-start

--- act.mk
+++ act.mk
@@ -1,8 +1,8 @@
-act-plebnet-playground:## 	run act -vbr -W .github/workflows/plebnet-playground.yml
-	@pushd $(PWD) && export $(cat ~/GH_TOKEN.txt) && act -vr -W .github/workflows/plebnet-playground.yml && popd
-act-nostr-rs-relay:## 	run act -vbr -W .github/workflows/nostr-rs-relay.yml
-	@pushd $(PWD) && export $(cat ~/GH_TOKEN.txt) && act -vr -W .github/workflows/nostr-rs-relay.yml && popd
-act-codeql-analysis:## 	run act -vbr -W .github/workflow/codeql-analysis.yml
-	@pushd $(PWD) && export $(cat ~/GH_TOKEN.txt) && act -vr -W .github/workflows/codeql-analysis.yml && popd
-act-codespell:## 	run act -vbr -W .github/workflow/codespell.yml
-	@pushd $(PWD) && export $(cat ~/GH_TOKEN.txt) && act -vr -W .github/workflows/codespell.yml && popd
+act-plebnet-playground:docker-start## 	run act -vbr -W .github/workflows/plebnet-playground.yml
+	@pushd $(PWD) && GITHUB_TOKEN=$(cat ~/GITHUB_TOKEN.txt) && act -vb -W .github/workflows/plebnet-playground.yml && popd
+act-nostr-rs-relay:docker-start## 	run act -vbr -W .github/workflows/nostr-rs-relay.yml
+	@pushd $(PWD) && GITHUB_TOKEN=$(cat ~/GITHUB_TOKEN.txt) && act -vb -W .github/workflows/nostr-rs-relay.yml && popd
+act-codeql-analysis:docker-start## 	run act -vbr -W .github/workflow/codeql-analysis.yml
+	@pushd $(PWD) && export $(cat ~/GITHUB_TOKEN.txt) && act -vb -W .github/workflows/codeql-analysis.yml && popd
+act-codespell:docker-start## 	run act -vbr -W .github/workflow/codespell.yml
+	@pushd $(PWD) && GITHUB_TOKEN=$(cat ~/GITHUB_TOKEN.txt) && act -vb -W .github/workflows/codespell.yml && popd
